aee4bbcf34f8cabb40027887e72b1da336ca0ac2
Seraj-Abu-Eisha <serajabueisha@gmail.com>
2026-01-14 19:22:58 +0200

    .

--- ModelValidationExample/Models/Person.cs
+++ ModelValidationExample/Models/Person.cs
@@ -4,12 +4,18 @@ namespace ModelValidationExample.Models
 {
     public class Person
     {
-        [Required(ErrorMessage = "PersonName can't be empty or null")]
+        [Required(ErrorMessage = "{0} PersonName can't be empty or null")]
+        [Display(Name = "Person name")]
+        [StringLength(40, MinimumLength = 3, 
+         ErrorMessage ="{0} should be between {2} and {1} chrachters long")]
+
         public string? PersonName { get; set; }
         public string? Email { get; set; }
         public string? Phone { get; set; }
         public string? Password { get; set; }
         public string? ConfirmPassword { get; set; }
+
+        [Range(0, 999.999, ErrorMessage ="{0} should be between ${1} and ${2}")]
         public double? Price { get; set; }
 
         public override string ToString()
