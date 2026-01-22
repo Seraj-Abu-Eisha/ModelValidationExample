fb3a05f9d78bf5b3ce5acee2003deaf0b3882ea4
Seraj-Abu-Eisha <serajabueisha@gmail.com>
2026-01-22 20:55:13 +0200

    .

---
+++ ModelValidationExample/CustomValidators/MinimumYearValidatorAttribute.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ModelValidationExample.CustomValidators
+{
+    public class MinimumYearValidatorAttribute : ValidationAttribute
+    {
+        public int MinimumYear { get; set; } = 2000;
+        public string DefualtErrorMessage { get; set; } =
+        "Date of birth should not be nwere than Jan 01, {0}";
+        public MinimumYearValidatorAttribute()
+        {
+
+        }
+        public MinimumYearValidatorAttribute(int minimumYear) 
+        {
+            MinimumYear = minimumYear;
+        }
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            if (value != null)
+            {
+                DateTime date = (DateTime)value;
+                if (date.Year >= MinimumYear)
+                {
+                    return new ValidationResult(string.Format(ErrorMessage?? 
+                        DefualtErrorMessage, MinimumYear));
+                }
+                else 
+                {
+                    return ValidationResult.Success; 
+                }
+            }
+            return null;
+        }
+    }
+}
--- ModelValidationExample/Models/Person.cs
+++ ModelValidationExample/Models/Person.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using ModelValidationExample.CustomValidators;
+using System.ComponentModel.DataAnnotations;
 
 namespace ModelValidationExample.Models
 {
@@ -6,17 +8,29 @@ namespace ModelValidationExample.Models
     {
         [Required(ErrorMessage = "{0} PersonName can't be empty or null")]
         [Display(Name = "Person name")]
-        [StringLength(40, MinimumLength = 3, 
-         ErrorMessage ="{0} should be between {2} and {1} chrachters long")]
-
+        [StringLength(40, MinimumLength = 3,
+         ErrorMessage = "{0} should be between {2} and {1} chrachters long")]
+        [RegularExpression("^[A-Za-z .]$", ErrorMessage = "{0} should only have contain only alphabets" +
+            ",space and dot (.)")]
         public string? PersonName { get; set; }
+        [EmailAddress(ErrorMessage = "{0} error should contain proper address")]
+        [Required(ErrorMessage ="{0} can't be left empty")]
         public string? Email { get; set; }
+        [Phone(ErrorMessage= "{0} Error, write a number contain 10 digits and no alphabets")]
+        //[ValidateNever]
         public string? Phone { get; set; }
+        [Required (ErrorMessage = "{0} make a proper password")]
         public string? Password { get; set; }
+        [Required(ErrorMessage = "{0} confirm the password")]
+        [Compare("Password", ErrorMessage ="{0} and {1} do not match")]
+        [Display(Name = "Re-Password")]
         public string? ConfirmPassword { get; set; }
 
         [Range(0, 999.999, ErrorMessage ="{0} should be between ${1} and ${2}")]
         public double? Price { get; set; }
+
+        [MinimumYearValidator(2005)]
+        public DateTime? DateOfBirth { get; set; }
 
         public override string ToString()
         {
